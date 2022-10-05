43a2a3a8a71bca750289351d8bd0a27a22417905
kathrynzaro <zarokathryn@gmail.com>
2022-10-04 17:29:47 -0700

    adds file structure / changes items table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS full_stack_todos;
+DROP TABLE IF EXISTS full_stack_items CASCADE;
 
 DROP TABLE IF EXISTS tasks;
 
-DROP TABLE IF EXISTS full_stack_users;
+DROP TABLE IF EXISTS full_stack_users CASCADE;
 
 CREATE TABLE full_stack_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -14,10 +14,12 @@ CREATE TABLE full_stack_users (
   last_name VARCHAR NOT NULL
 );
 
-CREATE TABLE full_stack_todos (
+CREATE TABLE full_stack_items (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT,
   completed BOOLEAN NOT NULL DEFAULT(FALSE),
-  description VARCHAR,
+  item VARCHAR,
+  quantity INT,
+  urgency VARCHAR,
   FOREIGN KEY (user_id) REFERENCES full_stack_users(id)
 );
